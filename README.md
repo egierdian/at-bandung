522f300255cd72b4f4a1869e4fd77f89e36ae5b5
egierdian <egierdian1@gmail.com>
2020-09-12 12:44:28 +0700

    Challenge DICODINGxIDCAMP

---
+++ README.md
@@ -0,0 +1,9 @@
+<h4>Demo:</h4>
+<a href="#">Here</a>
+<br />
+<h4>Dibuat: </h4>
+<ul>
+<li>Javascript</li>
+<li>Firebase</li>
+<li>Bootstrap 4</li>
+</ul>
---
+++ js/spinner.js
@@ -0,0 +1,5 @@
+document.getElementById('isi').style.display = 'none';
+setTimeout(function(){
+    document.getElementById('spinner').style.display = 'none';
+    document.getElementById('isi').style.display = 'block';
+}, 2000);
